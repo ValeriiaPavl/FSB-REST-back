7799bb975a24adce78bdadcb3695bd9a52d44b0c
Valeriia Pavlova <happyamanita@gmail.com>
2023-08-29 10:13:29 +0200

    added user_profile models to the admin panel (without that they were not displayed)

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from user_profile.models import UserInfo, InterestHashtag, Likes
 
+
+admin.site.register(UserInfo)
+admin.site.register(InterestHashtag)
+admin.site.register(Likes)
 # Register your models here.
