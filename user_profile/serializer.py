3e5e1ca89ce2226d6176f44dfc1010e3ff423921
Valeriia Pavlova <happyamanita@gmail.com>
2023-08-30 19:24:13 +0200

    changed serializer to get user id for redirecting to the user page in the future

--- user_profile/serializer.py
+++ user_profile/serializer.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
+
 from user_profile.models import UserInfo
 
 
 class UserInfoSerializer(serializers.ModelSerializer):
     username = serializers.ReadOnlyField(source='login.username')
     interest_hashtags = serializers.SerializerMethodField()
+    user_id = serializers.ReadOnlyField(source='login.id')
 
     class Meta:
         model = UserInfo
@@ -12,7 +14,7 @@ class UserInfoSerializer(serializers.ModelSerializer):
                   'city_of_residence_latitude',
                   'city_of_residence_longitude',
                   'year_of_birth', 'user_avatar',
-                  'user_description', 'interest_hashtags']
+                  'user_description', 'interest_hashtags', 'user_id']
 
     def get_interest_hashtags(self, obj):
         return [hashtag.name for hashtag in obj.interest_hashtags.all()]
