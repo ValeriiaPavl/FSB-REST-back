97b6f88cad6fa69ac5add75e04c53b57d2768c8c
Valeriia Pavlova <happyamanita@gmail.com>
2023-09-04 09:47:57 +0200

    changed imports a bit

--- user_profile/views.py
+++ user_profile/views.py
@@ -7,20 +7,20 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+import user_profile.serializer
 from FSB_REST.auth.authentication_utility import generate_jwt_token
 from FSB_REST.auth.middleware import JWTAuthentication
-from user_profile.models import UserInfo, Likes
-from user_profile.serializer import UserInfoSerializer, LikesSerializer, RegistrationSerializer, UserInfoPOSTSerializer
+from user_profile.models import UserInfo, Likes, InterestHashtag
 
 
 class UserInfoList(APIView):
     def get(self, request, format=None):
         user_infos = UserInfo.objects.select_related('login').prefetch_related('interest_hashtags').all()
-        serializer = UserInfoSerializer(user_infos, many=True)
+        serializer = user_profile.serializer.UserInfoSerializer(user_infos, many=True)
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = UserInfoPOSTSerializer(data=request.data)
+        serializer = user_profile.serializer.UserInfoPOSTSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -36,7 +36,7 @@ class UserDetail(APIView):
 
     def get(self, request, pk, format=None):
         user = self.get_object(pk)
-        serializer = UserInfoSerializer(user)
+        serializer = user_profile.serializer.UserInfoSerializer(user)
         return Response(serializer.data)
 
 
@@ -48,11 +48,11 @@ class LikesFromUserView(APIView):
         all_likes = Likes.objects.all()
         user_id = request.user.id
         person_likes = all_likes.filter(from_person=user_id)
-        serialized_likes_from = LikesSerializer(person_likes, many=True)
+        serialized_likes_from = user_profile.serializer.LikesSerializer(person_likes, many=True)
         return Response(serialized_likes_from.data)
 
     def post(self, request, from_person):
-        serializer = LikesSerializer(data=request.data)
+        serializer = user_profile.serializer.LikesSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -66,7 +66,7 @@ class LikesToUserView(APIView):
     def get(self, request):
         user_id = request.user.id
         who_liked_the_person = Likes.objects.filter(to_person=user_id)
-        serialized_likes_to = LikesSerializer(who_liked_the_person, many=True)
+        serialized_likes_to = user_profile.serializer.LikesSerializer(who_liked_the_person, many=True)
         return Response(serialized_likes_to.data)
 
 
@@ -81,7 +81,7 @@ class MutualLikesView(APIView):
         who_liked_the_person = all_likes.filter(to_person=user_id).values_list('from_person', flat=True)
         mutual_likes = person_likes.intersection(who_liked_the_person)
         user_infos = UserInfo.objects.filter(login_id__in=mutual_likes).prefetch_related("interest_hashtags")
-        serializer = UserInfoSerializer(user_infos, many=True)
+        serializer = user_profile.serializer.UserInfoSerializer(user_infos, many=True)
         print(serializer.data)
         return Response(serializer.data)
 
@@ -103,7 +103,7 @@ class LoginView(APIView):
 
 class RegisterView(APIView):
     def post(self, request):
-        serializer = RegistrationSerializer(data=request.data)
+        serializer = user_profile.serializer.RegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
