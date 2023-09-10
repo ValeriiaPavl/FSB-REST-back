023d86ba61e7de370882e12524940cca294cae47
Valeriia Pavlova <happyamanita@gmail.com>
2023-09-10 13:33:45 +0200

    changed token utility for more simple one

--- FSB_REST/auth/authentication_utility.py
+++ FSB_REST/auth/authentication_utility.py
@@ -1,5 +1,3 @@
-from datetime import datetime, timedelta
-
 import jwt
 from django.conf import settings
 
@@ -7,8 +5,8 @@ from django.conf import settings
 def generate_jwt_token(user_id):
     payload = {
         'user_id': user_id,
-        'exp': datetime.utcnow() + timedelta(days=14),
-        'iat': datetime.utcnow(),
+        # 'exp': datetime.utcnow() + timedelta(days=14),
+        # 'iat': datetime.utcnow(),
     }
     token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
     return token.decode('utf-8')
