06561035d735cb6c1c38244aef3c807c8425d231
Valeriia Pavlova <happyamanita@gmail.com>
2023-10-02 12:49:57 +0200

    added imports for connecting with prod db

--- FSB_REST/settings.py
+++ FSB_REST/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
 import environ
 
 env = environ.Env(DEBUG=(bool, False))
@@ -27,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in env
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fsb-backend.onrender.com"]
 
@@ -82,11 +83,8 @@ WSGI_APPLICATION = "FSB_REST.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+    'default': dj_database_url.config(
+        default=env('DATABASE_URL'), conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
